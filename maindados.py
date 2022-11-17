a4be2103e267af66b87ed11b762f806c12254c58
Pablo Moore <105167808+PabloMoore96@users.noreply.github.com>
2022-11-16 23:54:44 -0300

    dados version1.2
    menos líneas de código

--- maindados.py
+++ maindados.py
@@ -1,34 +1,15 @@
 import dados
 
+opcion=1
+while opcion != 0:
+    op=input("\n¿Cuantas caras quiere que tenga el dado?\n(0)Salir\n")
 
-if __name__=="__main__":
-    opcion=0
-    while opcion != 9:
-        op=input("\n¿Qué dado desea tirar?\n(1) Dado de 3 caras\n(2) Dado de 4 caras\n(3) Dado de 6 caras\n(4) Dado de 8 caras\n(5) Dado de 10 caras\n(6) Dado de 12 caras\n(7) Dado de 20 caras\n(8) Dado de 100 caras\n(9) Salir\n")
+    if op.isdigit():
 
-        if op.isdigit():
+        if opcion==0:
+            print("Hasta la próximaaa")
+        opcion=int(op)
+        dados.dados_tirar(opcion)   
 
-            opcion=int(op)
-            if opcion==1:
-                dados.dadod3()
-            elif opcion==2:
-                dados.dadod4()
-            elif opcion==3:
-                dados.dadod6()
-            elif opcion==4:
-                dados.dadod8()
-            elif opcion==5:
-                dados.dadod10()
-            elif opcion==6:
-                dados.dadod12()
-            elif opcion==7:
-                dados.dadod20()
-            elif opcion==8:
-                dados.dadod100()
-            elif opcion==9:
-                print("Hasta la próximaaa")
-            else:
-                print("Opción inválida.")
-
-        else:
-            print("Poner números.")
+    else:
+        print("Poner números.")
